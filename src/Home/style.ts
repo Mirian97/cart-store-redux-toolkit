50b9b2f0a41f2e01efc0b4692af651d3663e07ec
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-05-05 13:38:40 -0300

    style: complement styleof the home page

--- src/Home/style.ts
+++ src/Home/style.ts
@@ -1,10 +1,16 @@
-import { css, styled } from '@mui/material'
-import Box from '@mui/material/Box'
+import { Box, css, styled } from '@mui/material'
 
 export const StyledHomeContainer = styled(Box)`
   ${({ theme }) => css`
     background-color: ${theme.palette.background.default};
     width: 100%;
     min-height: 100vh;
+  `}
+`
+
+export const StyledHomeContent = styled(Box)`
+  ${({ theme }) => css`
+    max-width: 1440px;
+    margin: auto;
   `}
 `

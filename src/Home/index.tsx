b94b675979da9399f146c12aadef27fe19be750b
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-05-06 05:40:05 -0300

    feat:axios and async thunk to loaded the cart

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -2,21 +2,25 @@
 import CartContainer from 'components/CartContainer'
 import Header from 'components/Header'
 import Modal from 'components/Modal'
-import { calculateTotal } from 'features/cart/cartSlice'
+import { calculateTotal, getCartItems } from 'features/cart/cartSlice'
 import { closeModal } from 'features/modal/modalSlice'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { RootState } from 'store'
+import { AppDispatch, RootState } from 'store'
 import { StyledHomeContainer, StyledHomeContent } from './style'
 
 const Home = () => {
   const { cartItems } = useSelector((state: RootState) => state.cart)
   const { isOpen } = useSelector((state: RootState) => state.modal)
-  const dispatch = useDispatch()
+  const dispatch = useDispatch<AppDispatch>()
 
   useEffect(() => {
     dispatch(calculateTotal())
   }, [cartItems])
+
+  useEffect(() => {
+    dispatch(getCartItems())
+  }, [])
 
   return (
     <StyledHomeContainer>

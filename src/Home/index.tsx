bc73c6ff794563d3e510723a4100c0f3b3c2c08e
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-05-06 05:49:45 -0300

    feat: add loading whileloading the cart

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import CartContainer from 'components/CartContainer'
 import Header from 'components/Header'
+import Loading from 'components/Loading'
 import Modal from 'components/Modal'
 import { calculateTotal, getCartItems } from 'features/cart/cartSlice'
 import { closeModal } from 'features/modal/modalSlice'
@@ -10,7 +11,7 @@ import { AppDispatch, RootState } from 'store'
 import { StyledHomeContainer, StyledHomeContent } from './style'
 
 const Home = () => {
-  const { cartItems } = useSelector((state: RootState) => state.cart)
+  const { cartItems, isLoading } = useSelector((state: RootState) => state.cart)
   const { isOpen } = useSelector((state: RootState) => state.modal)
   const dispatch = useDispatch<AppDispatch>()
 
@@ -26,6 +27,7 @@ const Home = () => {
     <StyledHomeContainer>
       <Header />
       <StyledHomeContent>
+        <Loading open={isLoading} />
         <CartContainer />
       </StyledHomeContent>
       <Modal open={isOpen} closeModal={() => dispatch(closeModal())} />

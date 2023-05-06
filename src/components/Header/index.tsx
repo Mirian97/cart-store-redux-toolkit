12df1bcdb183960af49935fa6fa046f2c09bfa22
Mirian Montevilla <108299240+Mirian97@users.noreply.github.com>
2023-05-06 08:20:26 -0300

    style: modified style in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,27 +1,24 @@
-import LocalMallOutlinedIcon from '@mui/icons-material/LocalMallOutlined'
-import AppBar from '@mui/material/AppBar'
-import Badge from '@mui/material/Badge'
 import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
 import { useSelector } from 'react-redux'
 import { RootState } from 'store'
-import { StyledToolbar } from './style'
+import { StyledAppBar, StyledBadge, StyledBagIcon, StyledToolbar } from './style'
 
 const Header = () => {
   const amount = useSelector((store: RootState) => store.cart.amount)
   return (
-    <AppBar position='static'>
+    <StyledAppBar>
       <StyledToolbar>
         <Typography variant='h3' fontWeight={700}>
           Redux Toolkit
         </Typography>
         <IconButton color='inherit'>
-          <Badge badgeContent={amount} color='error'>
-            <LocalMallOutlinedIcon fontSize='large' />
-          </Badge>
+          <StyledBadge badgeContent={amount} color='error' showZero>
+            <StyledBagIcon />
+          </StyledBadge>
         </IconButton>
       </StyledToolbar>
-    </AppBar>
+    </StyledAppBar>
   )
 }
 
